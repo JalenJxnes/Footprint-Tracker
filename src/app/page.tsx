5bdb94938d04b133ab23cfca94ee0ec1b7322555
JalenJxnes <106790589+JalenJxnes@users.noreply.github.com>
2023-10-07 15:08:24 -0500

    Added calculator/ CTA component Began frontend design of CTA section Added new TailWind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Example from '../components/example';
+import Calculator from "../components/calculator";
+import '../app/global.css';
 
 export default function Home() {
   return (
-    <Example />
+    <div className='container min-h-screen h-[100vh]'>
+      <Example />
+      <Calculator />
+    </div>
   )
 }
