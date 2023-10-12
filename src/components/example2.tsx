6d9aa463a0cc9aff484cc8da466552d6f567d30c
jen1204 <142832650+jen1204@users.noreply.github.com>
2023-10-12 15:24:50 -0500

    Downloaded supporting files

--- src/components/example2.tsx
+++ src/components/example2.tsx
@@ -3,7 +3,7 @@ import '../app/global.css';
 export default function YourDesiredName() {
     return(
         <div className="container">
-
+            <h1>Hello world</h1>
         </div>
     )
 }
