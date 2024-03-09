0e12412031cfe8276aec85eb64ca207b6c6ffab0
JalenJxnes <jaljon1701@gmail.com>
2024-03-09 12:38:45 -0600

    Added tweaks to config file Removed styling from global.css Added new image in assets folder Tweaked functionality in 2 components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,23 @@
 'use client'
+import '../app/global.css';
+import Green_Energy from '../assets/green-energy.png'
+import Image from 'next/image';
 
 export default function Header() {
     return (
-        <div>
-            <div className="flex flex-col items-center ">
+        <div className='min-h-80'>
+            <div className="flex flex-col items-center min-h-96">
                 <h1 className="text-2xl">Carbon Footprint Tracker</h1>
                 <h2>Subheading here</h2>
+
+                <div>
+                    <Image
+                        src={Green_Energy}
+                        alt='Green Energy'
+                        width={500}
+                    >
+                    </Image>
+                </div>
             </div>
         </div>
     )
